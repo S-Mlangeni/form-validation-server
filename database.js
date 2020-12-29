2fb5cee77403abf0357da234ab8703772a2bac43
S-Mlangeni <smlangeni21@gmail.com>
2020-12-29 13:15:18 +0200

    first commit

---
+++ database.js
@@ -0,0 +1,73 @@
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+dotenv.config();
+
+//Creating a mysql connection:
+const connection = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DB,
+    port: process.env.DB_PORT
+})
+
+//Connecting with mysql db:
+connection.connect((error) => { //Call-back function is used to monitor the connection to the db.
+    if(error) {
+        console.error(error);
+    } else {
+        console.log ("DB connection is successful.")
+    }
+})
+
+const db_entry = (req, res) => {
+
+    // Inserting data using sql queries:
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const entry = `INSERT INTO formdata (username, email, password) VALUES ( "${username}", "${email}", "${password}")`;
+    connection.query(entry, (error) => {
+        if (error) {
+            console.error(error)
+        } else {
+            console.log("Data row is successfully added.")
+        }
+    })
+
+    res.end(); /* The "end()" method properly completes the request without sending an
+    actual response back to the client as this is not necessary since the registration page 
+    redirects after sending the user's data */
+
+}
+
+const db = (req, res) => {
+
+    // Selecting data using sql queries:
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const selection = `SELECT * FROM formdata WHERE password="${password}" AND username="${username}"`;
+    connection.query(selection, (error, result) => {
+        if (error) {
+            console.log(error);
+            res.send({status: "database query error!"})
+        } else if (result.length == 0) {
+            console.log("Data combination does not exist in database. Result is shown below.");
+            console.log(result);
+            res.send({outcome: false, status: "database query success, but data combination is not found."});
+        } else {
+            console.log("Data is successfully found in database. Result is shown below.");
+            console.log(result);
+            res.send({outcome: true, status: "database query success and data combination is found."});
+        }
+
+    })
+
+}
+
+module.exports = {
+    db_entry,
+    db
+}
---
+++ server.js
@@ -0,0 +1,63 @@
+// Our express server
+
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const dbController = require("./database") // "database" file in current/same directory/folder
+
+const express_app = express();
+
+express_app.listen(3000);
+
+
+/* express_app.set("view engine", "ejs"); *//* Sets the ejs view-engines/files as the 
+app's view-engine instead of static/plain-html files. */
+/* express_app.set("views", "ourviews") *//* Sets the "ourviews" folder as the app's views folder
+*/
+
+/*express_app.use(express.static("publicfiles")); *//* Middleware that uses the express package/
+module to load static files (i.e. css files, image files, etc.) from the specified folder
+to the server which can then be accessed by the server's associated files (i.e. ejs files). */
+/*express_app.use(express.urlencoded({ extended: true })); *//* Middleware function that connects the url data 
+(html data from ejs files) to the request object on the server file. */
+express_app.use(cors()); /* Middleware that activates the "cors" function to allow the client-side
+.js file to access the server side. Otherwise, file will be denied access to the server side
+by the browser. */ 
+express_app.use(express.json()); /* Middleware to help server read the posted/incomming data 
+in json format. Otherwise, requested data will result in an "undefined" result. */
+
+express_app.get("/", (request, response) => {
+    /*res.render("index"); *//* The "render()" method is used to send the specified ejs view to 
+    the browser/client-side. */
+    response.sendFile("client/index.html", {root: __dirname}); /* (file path and name, with respect 
+    to the specified directory (the "NodeJs Tutorial"/(project root) directory in this case)). 
+    {root: __dirname}" refers to the current directory used the by the project (i.e 
+    NODEJS TUTORIAL). */
+})
+
+express_app.post("/registration-API", dbController.db_entry /*(req, res) => {
+    console.log("client message is: " + req.body.myname);
+    console.log("Server route is activated!");
+    res.send({servermessage: "Got your post.", mymessage: req.body.myname, MyUserName: 
+    "Ben", MyPassword: 10});
+} */ )
+
+express_app.post("/login-API", dbController.db)
+
+express_app.get("/registration", (req, res) => {
+    res.render("registration");
+})
+
+express_app.use((req, res) => { /* Responses to a request that has missed the abovementioned 
+    requests (i.e. it acts as the default response to an unknown/unrecognized resquest).
+    Note that the server only returns a single response to the browser/client-side. */
+    res.status(404).send("<h1>Error!</h1> <p>Page is not found.</p>"); /* "404" is the actual
+    error status seen in the browser */
+})
+
+express_app.post("/registration", (req, res) => {
+    res.render("registration");
+})
+
+console.log("Our server is running")
